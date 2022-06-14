94d070fc92110d1fab4790b5fcfafc42418bc0ec
Aaron Li <aaronhli617@gmail.com>
2022-06-14 14:20:24 -0400

    tested fen

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-
-
-
 mod bit_operations;
 mod board;
 mod fen;
-mod move_generation; // generating moves
+mod move_generation; // generating bitboards
 
 fn main() {
 
-    let the_board = fen::from_fen("rnbqkbnr/ppppp1pp/8/4Pp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 2");
-    println!("{}", the_board.to_boardstring())
+    let the_board = fen::from_fen("rnbqkbnr/ppppp1pp/8/4Pp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 1 2");
+    println!("{}", the_board.to_fen())
 }
